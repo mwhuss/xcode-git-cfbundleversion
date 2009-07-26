3d035742d0083442402f582827da6b266c3bc0dc
digdog <digdog@mac.com>
2009-07-26 12:51:51 +0800

    update plist file only when git is available

--- xcode-git-cfbundleversion.rb
+++ xcode-git-cfbundleversion.rb
@@ -13,22 +13,23 @@ raise "Must be run from Xcode's Run Script Build Phase" unless ENV['XCODE_VERSIO
 # Get the current git revision hash
 revision = `/opt/local/bin/git rev-parse --short HEAD`.chomp!
 
-# Update Info.plist file
-plistFile = File.join(ENV['BUILT_PRODUCTS_DIR'], ENV['INFOPLIST_PATH'])
+if (revision)
+    # Update Info.plist file
+    plistFile = "#{ENV['BUILT_PRODUCTS_DIR']}/#{ENV['INFOPLIST_PATH']}"
 
-# Convert the binary plist to xml based
-`/usr/bin/plutil -convert xml1 #{plistFile}`
+    # Convert the binary plist to xml based
+    `/usr/bin/plutil -convert xml1 #{plistFile}`
 
-# Open Info.plist and set the CFBundleVersion value to the "CFBuildVersion (revision hash)" format
-lines = IO.readlines(plistFile).join
-lines.gsub!(/(<key>CFBundleVersion<\/key>\n\t<string>)(\d+\.\d+)(<\/string>)/, "\\1\\2 (#{revision})\\3")
- 
-# Overwrite the original Info.plist file with our updated version
-File.open(plistFile, 'w') {|f| f.puts lines}
+    # Open Info.plist and set the CFBundleVersion value to the "CFBuildVersion (revision hash)" format
+    lines = IO.readlines(plistFile).join
+    lines.gsub!(/(<key>CFBundleVersion<\/key>\n\t<string>)(\d+\.\d+)(<\/string>)/, "\\1\\2 (#{revision})\\3")
 
-# Convert back to binary plist
-`/usr/bin/plutil -convert binary1 #{plistFile}`
+    # Overwrite the original Info.plist file with our updated version
+    File.open(plistFile, 'w') {|f| f.puts lines}
 
-# Report to the user
-puts "CFBundleVersion in #{plistFile} sets to '#{revision}'"
+    # Convert back to binary plist
+    `/usr/bin/plutil -convert binary1 #{plistFile}`
 
+    # Report to the user
+    puts "CFBundleVersion has revision number #{revision} in #{plistFile}"
+end
